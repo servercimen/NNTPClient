9443a53b15b75129159915ae85f6638cf5510baf
cihancimen <cihan.cimen83@gmail.com>
2011-07-24 21:14:21 +0300

    Added newsgroup model

---
+++ NNTPClient/model/NewsGroup.h
@@ -0,0 +1,16 @@
+//
+//  NewsGroup.h
+//  NNTPClient
+//
+//  Created by cihancimen on 7/24/11.
+//  Copyright 2011 SpeedDate.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NewsGroup : NSObject {
+    
+}
+
+@end
---
+++ NNTPClient/model/NewsGroup.m
@@ -0,0 +1,14 @@
+//
+//  NewsGroup.m
+//  NNTPClient
+//
+//  Created by cihancimen on 7/24/11.
+//  Copyright 2011 SpeedDate.com. All rights reserved.
+//
+
+#import "NewsGroup.h"
+
+
+@implementation NewsGroup
+
+@end
